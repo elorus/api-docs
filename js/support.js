8cc875326366845f07113b95fd3f21702fff259d
George Stratakis <gstrat@elorus.com>
2017-06-16 13:16:16 +0300

    Update documentation

--- js/support.js
+++ js/support.js
@@ -1,13 +1,7 @@
-UserVoice=window.UserVoice||[];(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/6DoRuCZPIq2VowLHiOFdA.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})();
-UserVoice.push(['set', {accent_color: '#18bc9c', locale: 'en', menu_enabled: true, }]);
-(function($) {
-	$(document).ready(function() {
-		$('.support-contact-link').click(function () {
-			UserVoice.push(['show', {
-			  mode: 'contact',
-			  position: 'top-right'
-			}]);
-			return false;
-		});
-	});
-})(jQuery);
+<script>
+  window.intercomSettings = {
+    app_id: 'uyt0jfde',
+    custom_launcher_selector: '.support-contact-link',
+  };
+</script>
+<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/uyt0jfde';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
