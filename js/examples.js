5bf3afcb1e3e952bb641300c7b894bbc1bb0fa80
fergas <vagelis@elorus.com>
2022-10-04 15:58:38 +0300

    relative links changes, added different html for each example - css fixes, js fixes

--- js/examples.js
+++ js/examples.js
@@ -1,25 +1,5 @@
 const lists = Array.prototype.slice.call(document.getElementsByClassName('menu-item'))
 
-function init() {
-    toggleList(lists[0].id)
-}
-
-init();
-
-function toggleList(id) {
-    lists.forEach(function (list) {
-        if (list.id != id) {
-            list.parentElement.firstChild.style.background = "#f1f1f1"
-            list.parentElement.firstChild.style.color = "black";
-            list.style.display = "none";
-        } else {
-            list.parentElement.firstChild.style.background = '#263238';
-            list.parentElement.firstChild.style.color = '#ffffff';
-            list.style.display = "block";
-        }
-    })
-}
-
 //Get the button
 var mybutton = document.getElementById("myBtn");
 
