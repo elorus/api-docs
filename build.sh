9a1ed88c59644bd5059d73826493164f023ff07d
Pantelis Petridis <ppetrid@elorus.com>
2024-06-05 09:18:47 +0300

    - pull gh-pages before updating its contents

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ echo "building hugo public folder" &&
 hugo &&
 
 echo "checking out to: api-docs branch -> gh-pages" &&
-git checkout gh-pages &&
+git checkout gh-pages && git pull origin gh-pages &&
 
 echo "deleting all content except 'public' folder and git files" &&
 find . -maxdepth 1 -not \( -name '.' -or -name '..' -or -name '.gitignore' -or -name 'CNAME' -or -path './public*' -or -path './.idea*' -or -path './.git*' \) -exec rm -rf "{}" \; &&
